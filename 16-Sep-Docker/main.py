e3f3d0cff76d2e230a5a772cd1e6cc6f659c8e39
Yash Kalkani <yash.k.kombee@outlook.com>
2026-09-16 12:55:26 +0530

    ADD Docker file

---
+++ 16-Sep-Docker/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {"message": "Hello from Docker!"}
+
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
